5e8474ce62f8e3ab15d32476cb86ae1522cdfe4a
navruzbek200 <navrozbekbektemirov7@gmail.com>
2024-11-14 16:32:51 +0500

    enharitance practice

--- Enheritance.dart
+++ Enheritance.dart
@@ -1,23 +1,44 @@
 void main() {
-  Sherzod sherzod = Sherzod();
-  sherzod.printId("23452sd");
-  sherzod.printName("Sherzod");
+  Navruz navruz = Navruz();
+  navruz.printName("Navruz");
+  navruz.printAge(19);
+  navruz.printId(230403);
+  navruz.printCity("Samarkand");
+  navruz.printGroup(415);
+ 
 }
 
 class Student {
   void printName(String name) {
-    print("Name is $name");
+    print("My name is: $name");
   }
 
-  void printId(String id) {
-    print("Id is $id");
+  void printId(int id) {
+    print("Id is: $id");
   }
+
+  void printAge(int age) {
+    print(" age is: $age");
+  }
+
+
+  void printCity(String city) {
+    print("City is: $city");
+  }
+
+  void printGroup(int group) {
+    print("group is: $group");
+  }
+
+
 }
 
-class Sherzod extends Student {}
-
-class Hamid extends Student {}
 
 
+
+
+class Navruz extends Student {}
+class Shoxruz extends Student {}
+class Abdusalom extends Student {}
 
 
